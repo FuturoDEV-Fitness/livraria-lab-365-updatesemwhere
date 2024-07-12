4ae79057f5dc484030ff762bb71b4ab0b6bb9b0f
Lucas Pedro <lucaspedrolopescorrea@gmail.com>
2024-07-12 14:02:22 -0300

    Exercício 03

--- src/auditorios.js
+++ src/auditorios.js
@@ -1,4 +1,6 @@
 const readline = require('readline/promises');
+const Auditorio = require('./models/Auditorio');
+const AuditorioCrud = require('./models/AuditorioCrud');
 
 const rl = readline.createInterface({
     input: process.stdin,
@@ -6,21 +8,38 @@ const rl = readline.createInterface({
 });
 
 async function run() {
+    const auditorioCrud = new AuditorioCrud();
 
-    const resposta = await rl.question('Escolha uma ação (criar, deletar, alterar, consultar): ');
+    const resposta = await rl.question('Escolha uma ação (criar, deletar, consultar): ');
 
     switch (resposta) {
-        case 'criar':
-            /* Coloque sua resposta aqui  */
+        case 'criar': {
+            const codigo = await rl.question('Código: ');
+            const nome = await rl.question('Nome: ');
+            const descricao = await rl.question('Descrição: ');
+            const capacidade = await rl.question('Quantidade de pessoas suportado: ');
+
+            const auditorio = new Auditorio(codigo, nome, descricao, capacidade);
+            await auditorioCrud.criar(auditorio);
+            console.log('Auditório criado com sucesso!');
             rl.close();
             break;
+        }
         case 'deletar': {
-            /* Coloque sua resposta aqui */
+            const codigo = await rl.question('Código do auditório a deletar: ');
+            await auditorioCrud.deletar(codigo);
+            console.log('Auditório deletado com sucesso!');
             rl.close();
             break;
         }
         case 'consultar': {
-            /* Coloque sua resposta aqui */
+            const codigo = await rl.question('Código do auditório a consultar: ');
+            const auditorio = await auditorioCrud.consultar(codigo);
+            if (auditorio) {
+                console.log('Auditório encontrado:', auditorio);
+            } else {
+                console.log('Auditório não encontrado.');
+            }
             rl.close();
             break;
         }
@@ -28,7 +47,6 @@ async function run() {
             console.log("Ação não reconhecida.");
             rl.close();
     }
-
 }
 
 run();
--- src/classes/AuditorioCrud.js
+++ src/classes/AuditorioCrud.js
@@ -1,9 +1,45 @@
+const fs = require('fs/promises');
+const path = require('path');
+const Auditorio = require('./Auditorio');
+
 class AuditorioCrud {
-    
     constructor() {
-        this.filePath = './src/files/auditorios.json';
+        this.filePath = path.join(__dirname, '../files/auditorios.json');
     }
 
+    async lerArquivo() {
+        try {
+            const data = await fs.readFile(this.filePath, 'utf8');
+            return JSON.parse(data);
+        } catch (error) {
+            if (error.code === 'ENOENT') {
+                return [];
+            } else {
+                throw error;
+            }
+        }
+    }
+
+    async escreverArquivo(data) {
+        await fs.writeFile(this.filePath, JSON.stringify(data, null, 2));
+    }
+
+    async criar(auditorio) {
+        const auditorios = await this.lerArquivo();
+        auditorios.push(auditorio);
+        await this.escreverArquivo(auditorios);
+    }
+
+    async deletar(codigo) {
+        let auditorios = await this.lerArquivo();
+        auditorios = auditorios.filter(auditorio => auditorio.codigo !== codigo);
+        await this.escreverArquivo(auditorios);
+    }
+
+    async consultar(codigo) {
+        const auditorios = await this.lerArquivo();
+        return auditorios.find(auditorio => auditorio.codigo === codigo);
+    }
 }
 
 module.exports = AuditorioCrud;
