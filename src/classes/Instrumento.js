b6f43204ea87c55668a88b4ef6bb4d141bb1dbe3
Lucas Pedro <lucaspedrolopescorrea@gmail.com>
2024-07-12 14:22:12 -0300

    Exercício 05

--- src/classes/Instrumento.js
+++ src/classes/Instrumento.js
@@ -1,9 +1,10 @@
 class Instrumento {
-
-    constructor() {
-       
+    constructor(codigo, nome, tipo, estado) {
+        this.codigo = codigo;
+        this.nome = nome;
+        this.tipo = tipo;
+        this.estado = estado;
     }
-
 }
 
 module.exports = Instrumento;
--- src/instrumentos.js
+++ src/instrumentos.js
@@ -1,4 +1,6 @@
 const readline = require('readline/promises');
+const Instrumento = require('../models/Instrumento');
+const InstrumentoCrud = require('../models/InstrumentoCrud');
 
 const rl = readline.createInterface({
     input: process.stdin,
@@ -6,21 +8,38 @@ const rl = readline.createInterface({
 });
 
 async function run() {
+    const instrumentoCrud = new InstrumentoCrud();
 
-    const resposta = await rl.question('Escolha uma ação (criar, deletar, alterar, consultar): ');
+    const resposta = await rl.question('Escolha uma ação (criar, deletar, consultar): ');
 
     switch (resposta) {
-        case 'criar':
-            /* Coloque sua resposta aqui  */
+        case 'criar': {
+            const codigo = await rl.question('Código: ');
+            const nome = await rl.question('Nome: ');
+            const tipo = await rl.question('Tipo: ');
+            const estado = await rl.question('Estado: ');
+
+            const instrumento = new Instrumento(codigo, nome, tipo, estado);
+            await instrumentoCrud.criar(instrumento);
+            console.log('Instrumento criado com sucesso!');
             rl.close();
             break;
+        }
         case 'deletar': {
-            /* Coloque sua resposta aqui */
+            const codigo = await rl.question('Código do instrumento a deletar: ');
+            await instrumentoCrud.deletar(codigo);
+            console.log('Instrumento deletado com sucesso!');
             rl.close();
             break;
         }
         case 'consultar': {
-            /* Coloque sua resposta aqui */
+            const codigo = await rl.question('Código do instrumento a consultar: ');
+            const instrumento = await instrumentoCrud.consultar(codigo);
+            if (instrumento) {
+                console.log('Instrumento encontrado:', instrumento);
+            } else {
+                console.log('Instrumento não encontrado.');
+            }
             rl.close();
             break;
         }
@@ -28,7 +47,6 @@ async function run() {
             console.log("Ação não reconhecida.");
             rl.close();
     }
-
 }
 
 run();
